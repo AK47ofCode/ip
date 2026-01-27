85a259a512eb6fed07a1390aa3417a44a98036aa
Albert Lee Kuan Yi <2003lky@gmail.com>
2026-01-27 11:26:51 +0800

    Level 0: Add Rename, Greet, Exit
    Duke.java: Rename the Java file and add a few new features
    
    Duke.java is the default file provided in the project template and needs to be modified to make it more unique and boast more features.
    
    Renaming the file to Sallybot.java, as well as modifying the greeting message and the ASCII text that is displayed along with it and adding an exit message.
    
    The Duke Chatbot name is only a placeholder, and the project has since been renamed Sallybot, named after the Japanese-American voice actress Sally Amaki. Consequently, the greeting message has to be modified to reflect the change. A goodbye message is added to add more depth and friendliness.

---
+++ src/main/java/Sallybot.java
@@ -0,0 +1,16 @@
+public class Sallybot {
+    public static void main(String[] args) {
+        String logo = " ____   __   __    __    _  _  ____   __  ____ \n"
+                + "/ ___) / _\\ (  )  (  )  ( \\/ )(  _ \\ /  \\(_  _)\n"
+                + "\\___ \\/    \\/ (_/\\/ (_/\\ )  /  ) _ ((  O ) )(\n"
+                + "(____/\\_/\\_/\\____/\\____/(__/  (____/ \\__/ (__) \n";
+
+        System.out.println("____________________________________________________________\n");
+        System.out.println(logo + "\n");
+        System.out.println("Hola! I'm Sallybot!\n");
+        System.out.println("What can I do for you today?\n");
+        System.out.println("____________________________________________________________\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("___________________________________________________________\n");
+    }
+}
