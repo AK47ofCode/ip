9e0d9037fc26a9f19eeb1ba818162478d12778fe
Albert Lee Kuan Yi <2003lky@gmail.com>
2026-02-01 00:59:48 +0800

    Level 3: Mark as Done
    Add the ability to mark tasks as done and change the status back to not done.
    
    Previously, the task list had no capability of marking and displaying a task as completed or not.
    
    We add new class Task to create task objects that come with a description and a status that indicates the completion status of a task, as well as new commands "mark [index of task]" and "unmark [index of task]" to mark a task at a certain index as done or not done respectively.
    
    This adds more customizability and immersion to the Tasks feature of Sallybot.
    
    Minor additions such as Javadoc comments have been added.

--- src/main/java/Sallybot.java
+++ src/main/java/Sallybot.java
@@ -2,39 +2,100 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sallybot {
-    public static final int MAX_NUM_OF_TASKS = 100;
+//    public static final int MAX_NUM_OF_TASKS = 100;
 
     public static void main(String[] args) {
         boolean isStillExecuting = true;
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<>();
         String logo = getLogo();
 
         drawBorder();
         System.out.println(logo + "\n");
         System.out.println("\t 🌸こんにちは🌸\n");
-        System.out.println("\t Hello there✨ It's Sallybot! Always here to help hehe\n");
+        System.out.println("\t Hello there✨ I'm Sallybot! Always here to help hehe\n");
         System.out.println("\t What can I do for you today?\n");
         drawBorder();
 
         while (isStillExecuting) {
             Scanner input = new Scanner(System.in);
             String command = input.nextLine();
+            String[] commandArgs = command.trim().split("\\s+", 2);
+            String commandName = commandArgs[0];
 
-            switch (command) {
+            switch (commandName) {
             case "list":
-                drawBorder();
-                for (String task : tasks) {
-                    System.out.println("\t " + (tasks.indexOf(task) + 1) + ". " + task);
+                if (commandArgs.length == 1) {
+                    drawBorder();
+                    System.out.println("\t はい! Here are the tasks in your list:\n");
+                    for (Task task : tasks) {
+                        System.out.println("\t "
+                                + (tasks.indexOf(task) + 1) + ".["
+                                + task.getStatusIcon() + "] "
+                                + task.getDescription());
+                    }
+                    drawBorder();
+                    break;
                 }
-                drawBorder();
-                break;
+            case "mark":
+                if (commandArgs.length == 1) {
+                    System.out.println("\t すみません🙇‍♀️ Please provide the index of the task you would like to mark.\n");
+                    break;
+                }
+                else {
+                    try {
+                        int index = Integer.parseInt(commandArgs[1]);
+                        if (index >= 1 && index <= tasks.size()) {
+                            tasks.get(index - 1).markAsDone();
+                            drawBorder();
+                            System.out.println("\t はい! I've marked your task as done:\n");
+                            System.out.println("\t ["
+                                    + tasks.get(index - 1).getStatusIcon() + "] "
+                                    + tasks.get(index - 1).getDescription());
+                        }
+                        else  {
+                            drawBorder();
+                            System.out.println("\t すみません🙇‍♀️ This index is invalid!\n");
+                        }
+                        drawBorder();
+                        break;
+                    }
+                    catch (NumberFormatException ignored) {}
+                }
+            case "unmark":
+                if (commandArgs.length == 1) {
+                    System.out.println("\t すみません🙇‍♀️ " +
+                            "Please provide the index of the task you would like to unmark.\n");
+                    break;
+                }
+                else {
+                    try {
+                        int index = Integer.parseInt(commandArgs[1]);
+                        if (index >= 1 && index <= tasks.size()) {
+                            tasks.get(index - 1).markAsNotDone();
+                            drawBorder();
+                            System.out.println("\t はい! I've marked your task as not done:\n");
+                            System.out.println("\t ["
+                                    + tasks.get(index - 1).getStatusIcon() + "] "
+                                    + tasks.get(index - 1).getDescription());
+                        }
+                        else  {
+                            drawBorder();
+                            System.out.println("\t すみません🙇‍♀️ This index is invalid!\n");
+                        }
+                        drawBorder();
+                        break;
+                    }
+                    catch (NumberFormatException ignored) {}
+                }
             case "bye":
-                isStillExecuting = false;
-                break;
+                if (commandArgs.length == 1) {
+                    isStillExecuting = false;
+                    break;
+                }
             default:
-                tasks.add(command);
+                tasks.add(new Task(command));
                 drawBorder();
-                System.out.println("\t Added into the list of tasks: " + command + "\n");
+                System.out.println("\t はい! Added into the list of tasks: " + command + "\n");
                 drawBorder();
                 break;
             }
@@ -43,6 +104,9 @@ public class Sallybot {
         printByeMessage();
     }
 
+    /**
+     * Prints the bye message
+     */
     private static void printByeMessage() {
         drawBorder();
         System.out.println("\t じゃあね👋\n");
@@ -50,10 +114,16 @@ public class Sallybot {
         drawBorder();
     }
 
+    /**
+     * Draws a border
+     */
     private static void drawBorder() {
         System.out.println("\t____________________________________________________________\n");
     }
 
+    /**
+     * @return Displays the ASCII logo of Sallybot
+     */
     private static String getLogo() {
         return """
                 \t  ____   __   __    __    _  _  ____   __  ____\s
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+/**
+ * Creates an object that represents a Task<br>
+ * It stores the description and the status of the task (whether it is done or not)
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
+    }
+}
