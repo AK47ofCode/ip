42203925e52eca23dc6ec1d6e5fc047954d2d71e
Albert Lee Kuan Yi <2003lky@gmail.com>
2026-01-31 19:15:05 +0800

    Level 1: Add Echo and command messages
    Sallybot.java: Repeat a string message that a user types in the CLI, sends the goodbye message when the command "bye" is entered.
    
    The previous iteration of Sallybot only had hello and bye messages and lacked interaction with the user via user input.
    
    Here, we add a Scanner input that takes a string entered by the user, and prints that exact same string on the CLI. We add a while loop to constantly prompt the user to enter a message until the command "bye" is sent. We use a case statement to handle the commands entered by the user, and the case block handles the break from the while loop when "bye" is entered such that the program quits.
    
    To provide users with the choice of constantly entering messages without needing to restart the application each time a message is entered, a while loop is used. We expect more commands to be introduced, so a case statement is utilised.
    
    In addition, the content of the greeting and bye messages have been modified to better reflect the intended character of Sallybot, which follows the personality of its namesake.

--- src/main/java/Sallybot.java
+++ src/main/java/Sallybot.java
@@ -1,16 +1,52 @@
+import java.util.Scanner;
+
 public class Sallybot {
     public static void main(String[] args) {
-        String logo = " ____   __   __    __    _  _  ____   __  ____ \n"
-                + "/ ___) / _\\ (  )  (  )  ( \\/ )(  _ \\ /  \\(_  _)\n"
-                + "\\___ \\/    \\/ (_/\\/ (_/\\ )  /  ) _ ((  O ) )(\n"
-                + "(____/\\_/\\_/\\____/\\____/(__/  (____/ \\__/ (__) \n";
-
-        System.out.println("____________________________________________________________\n");
+        boolean isStillExecuting = true;
+        String logo = getLogo();
+        drawBorder();
         System.out.println(logo + "\n");
-        System.out.println("Hola! I'm Sallybot!\n");
-        System.out.println("What can I do for you today?\n");
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("___________________________________________________________\n");
+        System.out.println("\t🌸こんにちは🌸\n");
+        System.out.println("\tHello there✨ It's Sallybot! Always here to help hehe\n");
+        System.out.println("\tWhat can I do for you today?\n");
+        drawBorder();
+
+        while (isStillExecuting) {
+            Scanner input = new Scanner(System.in);
+            String command = input.nextLine();
+
+            switch (command) {
+            case "bye":
+                isStillExecuting = false;
+                break;
+            default:
+                drawBorder();
+                System.out.println("\t" + command + "\n");
+                drawBorder();
+                break;
+            }
+        }
+
+        printByeMessage();
+    }
+
+    private static void printByeMessage() {
+        drawBorder();
+        System.out.println("\tじゃあね👋\n");
+        System.out.println("\tSee you later!\n");
+        drawBorder();
+    }
+
+    private static void drawBorder() {
+        System.out.println("\t____________________________________________________________\n");
+    }
+
+    private static String getLogo() {
+        return """
+                \t ____   __   __    __    _  _  ____   __  ____\s
+                \t/ ___) / _\\ (  )  (  )  ( \\/ )(  _ \\ /  \\(_  _)
+                \t\\___ \\/    \\/ (_/\\/ (_/\\ )  /  ) _ ((  O ) )(
+                \t(____/\\_/\\_/\\____/\\____/(__/  (____/ \\__/ (__)\s
+                """;
     }
 }
